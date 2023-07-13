446d81a9cdc9b2f7644d3a81d5bf3d56684b3f45
Jesus Akanle <jesuswrites20043@gmail.com>
2023-07-13 10:32:54 +0000

    I set the home

---
+++ Deliva/src/components/signUp.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function signUp () {
+//   const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello! This is the Sign Up Page.</h1>
+    </>
+  )
+}
+
+export default signUp
