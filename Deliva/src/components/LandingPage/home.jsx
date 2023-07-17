ce046d0f7112e1c2592545896ffc767a8f65b731
Jesus Akanle <jesuswrites20043@gmail.com>
2023-07-17 13:54:24 +0000

    CAdded Sign In Component

---
+++ Deliva/src/components/LandingPage/home.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+function signUp () {
+//   const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello! This is the Landing Page.</h1>
+
+<button>Sign Up</button>
+    </>
+  )
+}
+
+export default signUp
