488114e4b1962aebc15ee2fa034dc1d7a1d3783c
Akanle Jesus <jesuswrites20043@gmail.com>
2023-07-18 11:25:14 +0000

    Instantiated Footer

---
+++ Deliva/src/components/LandingPage/Footer.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+function Footer () {
+    return (
+        <>
+          <h1>This is the Footer Page!</h1>
+        </>
+    )
+}
+
+export default Footer
---
+++ Deliva/src/components/LandingPage/Hero.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+function Hero () {
+    return (
+        <>
+          <h1>This is the Hero Page!</h1>
+        </>
+    )
+}
+
+export default Hero
