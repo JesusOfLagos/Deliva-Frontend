91cd9c8401df116510559cf0239d368ee62da910
Dot Dev <thejesusoflagos@gmail.com>
2023-07-22 11:49:43 +0000

    I started Navbar

---
+++ deliva/app/components/Navbar/navbar.tsx
@@ -0,0 +1,11 @@
+function Navbar () {
+    return (
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+            <div className="py-4 border-b-[1px]">
+                <Container />
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
