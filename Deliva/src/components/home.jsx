7719c4b9b35c32cda0f2d2fb16f48a7d7fca1b1f
JesusDotDev <139908031+JesusDotDev@users.noreply.github.com>
2023-07-24 17:25:54 +0100

    Update home.jsx

--- Deliva/src/components/home.jsx
+++ Deliva/src/components/home.jsx
@@ -1,1 +1,15 @@
+import Footer from "./LandingPage/footer"
+import Hero from "./LandingPage/hero"
+import Navbar from "./LandingPage/navbar"
 
+function Home () {
+    return (
+        <>
+        <Navbar />
+        <Hero />
+        <Footer />
+        </>
+    )
+}
+
+export default Home
