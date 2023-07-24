1bd615918eef083aff8be438c9dc7a598d14b342
JesusDotDev <139908031+JesusDotDev@users.noreply.github.com>
2023-07-24 17:14:09 +0100

    Update App.jsx

--- Deliva/src/App.jsx
+++ Deliva/src/App.jsx
@@ -1,29 +1,33 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import SignUp from './components/Auth/signUp';
-import SignIn from './components/Auth/signIn';
-import ResetPassword from './components/Auth/resetPassword';
-import ForgotPassword from './components/Auth/forgotPassword';
-import Navbar from './components/LandingPage/Navbar';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ResetPassword from "./components/Auth/resetPassword";
+import ForgotPassword from "./components/Auth/forgotPassword";
+import SignIn from "./components/Auth/signIn";
+import NotFound from "./components/Auth/notFound";
+import Home from "./components/home";
+import SignUp from "./components/Auth/signUp";
+import Dashboard from "./components/dashboard";
+import "./App.css";
 
-function App () {
+
+
+export default function App() {
   return (
     <>
-      <div className="card">
-      <Navbar />
-      <SignIn />
-      </div>
-      {/* <BrowserRouter>
-        <Switch>
-          <Route path="/sign-in" component={SignIn} />
-          <Route path="/sign-up" component={SignUp} />
-          <Route path="/reset-password" component={ResetPassword} />
-          <Route path="/forgot-password" component={ForgotPassword} />
-          <Route path="/" component={Navbar} />
-        </Switch>
-      </BrowserRouter> */}
+      <Router>
+        <Routes>
+          {/* Define your routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+
+          {/* If no route matches, show the NotFound component */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </>
   );
 }
-
-export default App
