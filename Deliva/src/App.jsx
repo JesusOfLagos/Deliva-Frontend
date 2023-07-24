4a9317cc473e3eb6939ac7bec1d9e4d323dcd81b
Dot Dev <thejesusoflagos@gmail.com>
2023-07-24 13:00:11 +0000

    I added new routes

--- Deliva/src/App.jsx
+++ Deliva/src/App.jsx
@@ -1,27 +1,47 @@
 // import { useState } from 'react'
 // import { Link } from 'react-router-dom'
-// import SignUp from './components/Auth/signUp'
-// import SignIn from './components/Auth/signIn'
-// import ResetPassword from './components/Auth/resetPassword'
-// import ForgotPassword from './components/Auth/forgotPassword'
+import SignUp from './components/Auth/signUp'
+import SignIn from './components/Auth/signIn'
+import ResetPassword from './components/Auth/resetPassword'
+import ForgotPassword from './components/Auth/forgotPassword'
 import Navbar from './components/LandingPage/Navbar'
 // import Hero from '../src/components/LandingPage/Hero'
 // import Footer from '../src/components/LandingPage/Footer'
 import './App.css'
 
 
-function App() {
-  return (
-    <>
-      <div className="card">
-      <Navbar />
+// function App() {
+  // return (
+    // <>
+      {/* <div className="card"> */}
+      {/* <Navbar /> */}
       {/* <Hero /> */}
       {/* <Footer /> */}
       {/* <SignIn /> */}
       {/* <SignUp /> */}
       {/* <ResetPassword /> */}
       {/* <ForgotPassword /> */}
-      </div>
+      {/* </div> */}
+    {/* </> */}
+  // )
+// }
+
+
+
+
+
+function App() {
+  return (
+    <>
+     <Routes>
+      <Route>
+        <Route path ="/sign-in" element = {<SignIn />} />
+        <Route path ="/sign-up" element = {<SignUp />} />
+        <Route path ="/sign-in" element = {<ResetPassword />} />
+        <Route path ="/sign-in" element = {<ForgotPassword />} />
+        <Route path ="/sign-in" element = {<Navbar />} />
+      </Route>
+     </Routes>
     </>
   )
 }
