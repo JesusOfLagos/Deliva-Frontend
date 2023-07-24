b356f3c4fe92c3664c70b6a9cd7ce74e96c3301d
Dot Dev <thejesusoflagos@gmail.com>
2023-07-24 13:56:39 +0000

    Updated routes

--- Deliva/src/App.jsx
+++ Deliva/src/App.jsx
@@ -1,49 +1,29 @@
-// import { useState } from 'react'
-// import { Link } from 'react-router-dom'
-import SignUp from './components/Auth/signUp'
-import SignIn from './components/Auth/signIn'
-import ResetPassword from './components/Auth/resetPassword'
-import ForgotPassword from './components/Auth/forgotPassword'
-import Navbar from './components/LandingPage/Navbar'
-// import Hero from '../src/components/LandingPage/Hero'
-// import Footer from '../src/components/LandingPage/Footer'
-import './App.css'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import SignUp from './components/Auth/signUp';
+import SignIn from './components/Auth/signIn';
+import ResetPassword from './components/Auth/resetPassword';
+import ForgotPassword from './components/Auth/forgotPassword';
+import Navbar from './components/LandingPage/Navbar';
+import './App.css';
 
-
-// function App() {
-  // return (
-    // <>
-      {/* <div className="card"> */}
-      {/* <Navbar /> */}
-      {/* <Hero /> */}
-      {/* <Footer /> */}
-      {/* <SignIn /> */}
-      {/* <SignUp /> */}
-      {/* <ResetPassword /> */}
-      {/* <ForgotPassword /> */}
-      {/* </div> */}
-    {/* </> */}
-  // )
-// }
-
-
-
-
-
-function App() {
+function App () {
   return (
     <>
-     <Routes>
-      <Route>
-        <Route path ="/sign-in" element = {<SignIn />} />
-        <Route path ="/sign-up" element = {<SignUp />} />
-        <Route path ="/sign-in" element = {<ResetPassword />} />
-        <Route path ="/sign-in" element = {<ForgotPassword />} />
-        <Route path ="/sign-in" element = {<Navbar />} />
-      </Route>
-     </Routes>
+      <div className="card">
+      <Navbar />
+      <SignIn />
+      </div>
+      {/* <BrowserRouter>
+        <Switch>
+          <Route path="/sign-in" component={SignIn} />
+          <Route path="/sign-up" component={SignUp} />
+          <Route path="/reset-password" component={ResetPassword} />
+          <Route path="/forgot-password" component={ForgotPassword} />
+          <Route path="/" component={Navbar} />
+        </Switch>
+      </BrowserRouter> */}
     </>
-  )
+  );
 }
 
 export default App
