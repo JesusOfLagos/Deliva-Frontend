249682424942fb778b3df6b0e783e82ab46aea95
Dot Dev <thejesusoflagos@gmail.com>
2023-07-25 13:49:34 +0000

    Completed Hero

--- Deliva/src/components/LandingPage/hero.jsx
+++ Deliva/src/components/LandingPage/hero.jsx
@@ -2,14 +2,14 @@ import heroImage from "../../assets/heroImage.png"
 
 export default function Hero () {
     return (
-        <div className="flex flex-row">
-            <div>
-                <img className="h-60 w-80" src={heroImage} alt="heroImage" />
+        <div className="flex flex-row mt-32 mb-32 space-x-6">
+            <div className="flex-1 h-90">
+                <img className="" src={heroImage} alt="heroImage" />
             </div>
-            <div>
-                <h1>Experience fast <br /> delivery with our <br /> technology driven <br /> approach</h1>
-                <p>Our goal is to provide a cost-effective and convenient solution for small businesses, enabling them to compete with larger players in the market.</p>
-                <button className="bg-black text-white -p-2">Get our App</button>
+            <div className=" flex-1">
+                <h1 className="text-black text-left">Experience fast <br /> delivery with our <br /> technology driven <br /> approach</h1>
+                <p className="text-black text-left mt-6">Our goal is to provide a cost-effective and <br /> convenient solution for small businesses, enabling <br /> them to compete with larger players in the market.</p>
+                <button className="bg-black text-white float-left mt-4 rounded-full p-[-2]">Get our App</button>
             </div>
         </div>
     );
