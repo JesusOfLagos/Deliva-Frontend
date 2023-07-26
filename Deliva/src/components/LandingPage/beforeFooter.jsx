bba3181e29f3ebdf79c02de89517c94d4ef10644
Dot Dev <thejesusoflagos@gmail.com>
2023-07-26 10:14:15 +0000

    I added flex to BeforeFooter

--- Deliva/src/components/LandingPage/beforeFooter.jsx
+++ Deliva/src/components/LandingPage/beforeFooter.jsx
@@ -8,11 +8,11 @@ import footerMap from "./../../assets/footerMap.png"
 
 export default function BeforeFooter () {
     return (
-        <div className="flex flex-row mt-32 mb-32 space-x-6">
-            <div>
+        <div className="flex bg-gray-500 flex-row mt-32 mb-32 space-x-6">
+            <div className="flex-1">
                 <img src={footerMap} alt="" />
             </div>
-            <div>
+            <div className="flex-1">
                 <div>
                     <h2>Download <br /> Deliva Pro Now</h2>
                 </div>
