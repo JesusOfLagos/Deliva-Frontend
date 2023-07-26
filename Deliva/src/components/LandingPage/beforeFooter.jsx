6cdd2ade98262f4ede5ff3d79fa0efa0ad96367c
Dot Dev <thejesusoflagos@gmail.com>
2023-07-26 10:24:35 +0000

    I added googlePlay and AppleStore

--- Deliva/src/components/LandingPage/beforeFooter.jsx
+++ Deliva/src/components/LandingPage/beforeFooter.jsx
@@ -15,8 +15,28 @@ export default function BeforeFooter () {
             <div className="flex-1">
                 <div>
                     <h2>Download <br /> Deliva Pro Now</h2>
+                    <p>Get Started and deliva your packages safely and faster. Get Deliva pro now.</p>
                 </div>
-                <div></div>
+                <div>
+                    <div className="bg-white">
+                        <div>
+                        <img src={googlePlay} alt="googlePlay" />
+                        </div>
+                        <div>
+                        <p>Get it on</p>
+                        <h2>Google Play</h2>
+                        </div>
+                    </div>
+                    <div className="bg-white flex-row">
+                        <div>
+                        <img src={Apple} alt="Apple Store" />
+                        </div>
+                        <div>
+                        <p>Get it on</p>
+                        <h2>Apple Store</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
